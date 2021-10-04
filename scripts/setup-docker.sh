5f3ee32ea177548184fd5a9ab0f1b980481345e5
jamochl <james.lim@jamochl.com>
2021-10-04 22:33:32 +1100

    add section to install docker-compose and check version of both

--- scripts/setup-docker.sh
+++ scripts/setup-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
+# Install Docker
 sudo apt update && sudo apt upgrade -y
-
 sudo apt install -y ca-certificates \
     curl \
     gnupg \
@@ -14,5 +14,15 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io
-
 sudo usermod -aG docker $USER
+
+# Install Docker-Compose V2
+
+sudo mkdir -p /usr/local/lib/docker/cli-plugins
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.0.0/docker-compose-linux-amd64 -o /usr/local/lib/docker/cli-plugins/docker-compose
+sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+
+# Check versions
+
+docker --version
+docker compose --version
